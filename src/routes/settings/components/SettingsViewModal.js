d7b44cc6bbef2973b6c30e7359346bd36ac016d7
Simple-codinger <vincentgierisch@gmail.com>
2018-10-26 23:34:50 +0200

    remove whitespace

--- src/routes/settings/components/SettingsViewModal.js
+++ src/routes/settings/components/SettingsViewModal.js
@@ -4,7 +4,6 @@ import * as React from 'react'
 import { View, Text } from 'react-native'
 import { CheckBox } from 'react-native-elements'
 
-
 export default class SettingsViewModal extends React.Component {
 
   render () {
